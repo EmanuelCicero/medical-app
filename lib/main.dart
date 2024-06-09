bf7bb8c14190c3591bc4bfce94069f1db6f6c0c5
Emanuel <emanuelcicero40@gmail.com>
2024-06-09 16:06:01 -0300

    :tada: flutter project :tada:

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'bla bla bla!',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+} 
